2515211c3821bc517ccc402ae865470962772911
unknown <thiago.developer.rj@gmail.com>
2020-05-15 23:56:17 -0300

    Projeto Criado

---
+++ src/test/java/com/thiagovieira/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagovieira.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
