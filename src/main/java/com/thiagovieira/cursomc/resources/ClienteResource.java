834a5caa47704021270f4e7bbe0d5b21bb0d2a9c
unknown <thiago.developer.rj@gmail.com>
2020-12-20 17:34:33 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/thiagovieira/cursomc/resources/ClienteResource.java
@@ -0,0 +1,31 @@
+package com.thiagovieira.cursomc.resources;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.thiagovieira.cursomc.domain.Cliente;
+import com.thiagovieira.cursomc.services.ClienteService;
+
+import javassist.tools.rmi.ObjectNotFoundException;
+
+@RestController
+@RequestMapping(value = "/clientes")
+public class ClienteResource {
+
+	@Autowired
+	private ClienteService service;
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> findbyid(@PathVariable Integer id) throws ObjectNotFoundException {
+		
+		Optional<Cliente> obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+		
+	}
+}
---
+++ src/main/java/com/thiagovieira/cursomc/services/ClienteService.java
@@ -0,0 +1,27 @@
+package com.thiagovieira.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thiagovieira.cursomc.domain.Cliente;
+import com.thiagovieira.cursomc.repositories.ClienteRepository;
+import com.thiagovieira.cursomc.services.exception.ObjectNotFoundException;
+
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Optional<Cliente> buscar(Integer id)  {
+		Optional<Cliente> obj = repo.findById(id);
+		if(obj == null) {
+		throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName());
+		}
+		return obj;
+	}
+	
+}
